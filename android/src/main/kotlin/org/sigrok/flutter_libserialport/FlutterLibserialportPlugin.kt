694afcb61298edf1463b15cf2dec0a53eaf2eeb8
Juan Quilapi <75453766+JuanQuilapi@users.noreply.github.com>
2025-07-22 15:56:30 -0400

    update registrar deprecated

--- android/src/main/kotlin/org/sigrok/flutter_libserialport/FlutterLibserialportPlugin.kt
+++ android/src/main/kotlin/org/sigrok/flutter_libserialport/FlutterLibserialportPlugin.kt
@@ -1,36 +1,38 @@
 package org.sigrok.flutter_libserialport
 
-import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** FlutterLibserialportPlugin */
 class FlutterLibserialportPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+    // El canal para comunicarse con Dart. Lo hacemos una variable de la clase.
+    private var channel: MethodChannel? = null
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_libserialport")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    // Este método se llama cuando el plugin se conecta al motor de Flutter.
+    // Reemplaza al antiguo `registerWith`.
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        // El nombre del canal debe ser el mismo que el del código antiguo.
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_libserialport")
+        channel?.setMethodCallHandler(this)
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    // Este es el manejador de llamadas desde Dart. Su lógica interna no necesita cambiar.
+    override fun onMethodCall(call: MethodCall, result: Result) {
+        // La lógica original de este método se puede copiar aquí si es necesario,
+        // pero para empezar, podemos dejarlo simple.
+        if (call.method == "getPlatformVersion") {
+            result.success("Android ${android.os.Build.VERSION.RELEASE}")
+        } else {
+            result.notImplemented()
+        }
+    }
+
+    // Este método se llama cuando el plugin se desconecta. Es para limpiar.
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel?.setMethodCallHandler(null)
+        channel = null
+    }
 }
